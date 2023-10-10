823f01e903325f68902adaabee3f76ec92853364
Ashwini Sharma <90304035+Xordy-Codes@users.noreply.github.com>
2023-10-10 21:39:03 +0530

    Add files via upload

---
+++ Strings/String_functions.js
@@ -0,0 +1,40 @@
+//Lenght 
+/*const sentence = "From Now Javascript is my Primary Language"; //indexing starts from 0
+console.log(`using of lenght function we get the lenght of the given senetence is ${sentence.length}`);*/
+
+
+//charAt(): gives you character at given at any given index.
+/*const sentence = "From Now Javascript is my Primary Language";
+let index = 5;
+console.log(`using of ${index} we get from senetence is character ${sentence.charAt(index)}`);*/
+
+
+//codePointAt(): gives you ascii code of given character.
+/*const sentence = "From Now Javascript is my prmary language";
+let index = 6; 
+console.log(`The character code of ${sentence.charAt(index)} is equal to ${sentence.codePointAt(index)}`);*/
+
+
+//concat(): Concatination of 2 Strings.
+/*const sentence1 = "From Now Javascript is my prmary language.";
+const sentence2 = " But my previous language was Core Java.";
+console.log(`By using concat function the sentence will become: ${sentence1.concat(sentence2)}`);*/
+
+
+//endswith(): The endsWith() method of String values determines whether a string ends with the characters of this string, returning true or false as appropriate.
+/*const sentence = "From Now Javascript is my prmary language";
+console.log(sentence.endsWith("language"));
+console.log(sentence.endsWith("Javascript"));*/
+
+
+//includes(): The includes() method of String values performs a case-sensitive search to determine whether a given string may be found within this string, returning true or false as appropriate.
+/*const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox';
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+// Expected output: "The word "fox" is in the sentence"*/
+
+
+
+
+
+
